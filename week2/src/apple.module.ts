c5ea4719719f43d3a2404aa5d6d12dc05abf8935
허민지 <mjhuh263@gmail.com>
2022-10-12 19:03:47 +0900

    fix: 문제 정답 및 설명

--- week2/src/apple.module.ts
+++ week2/src/apple.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppleService } from './apple.service';
 
+/**
+ * `exports`으로 AppleService도 외부에 접근 가능한 상태
+ * - module provider => 1:n 관계
+ *   - 일을 하는 주체는 provider (하나의 모듈에 속해 있는)
+ *   - provider가 다른 provider와 같이 일을 해야하는 경우, module을 export해서 module 전체를 갖다 쓴다
+ */
 @Module({
   providers: [AppleService],
+  exports: [AppleService],
 })
 export class AppleModule {}
--- week2/src/banana.module.ts
+++ week2/src/banana.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppleModule } from './apple.module';
 import { BananaService } from './banana.service';
 
 @Module({
+  imports: [AppleModule],
   providers: [BananaService],
 })
 export class BananaModule {}
